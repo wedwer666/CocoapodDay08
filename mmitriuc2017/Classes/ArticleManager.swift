f6e42c34f9749d3e1606f19c9263adb9afeb1ff8
mmitriuc6 <mmitriuc6@e1p9.chisinau.42.fr>
2017-05-03 15:01:59 +0300

    hell0

---
+++ mmitriuc2017/Classes/ArticleManager.swift
@@ -0,0 +1,212 @@
+//
+//  ArticleManager.swift
+//  Pods
+//
+//  Created by Mitriuc Maria on 4/28/17.
+//
+//
+
+import Foundation
+import CoreData
+
+//class ViewController: UIViewController
+//{
+//    let appDelegate = NSApplication.shared().delegate as! AppDelegate
+//    let managedObjectContext = appDelegate.managedObjectContext
+//   // var managedObjectContext
+//    
+//    let article = NSEntityDescription.insertNewObjectForEntityForName("Article", inManagedObjectContext: managedObjectContext) as! Article
+//
+//   // let article = NSEntityDescription.entityForName("Article", inManagedObjectContext: self.managedObjectContext)
+//  //  let articleManager = ArticleManager()
+//    
+//        override func viewDidLoad()
+//        {
+//            
+//            //let firstName = "Trevor"
+//           // fetchRequest.predicate = NSPredicate(format: "firstName == %@", firstName)
+//            
+//            func newArticle(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+//            {
+//                let entityDescription = NSEntityDescription.entityForName("Article", inManagedObjectContext: self.managedObjectContext)
+//                let managedObject = NSManagedObject(entity: entityDescription!, insertIntoManagedObjectContext: self.managedObjectContext)
+//                
+//                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Article")
+//                let results = try self.managedObjectContext.executeFetchRequest(fetchRequest)
+//                for result in results as! [NSManagedObject] {
+//                    print("name - \(result.valueForKey("name")!)")
+//            }
+//                
+//           //withLang lang: String
+//           func getAllArticles(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+//           {
+//                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Article")
+//                    do {
+//                        let results = try self.managedObjectContext.executeFetchRequest(fetchRequest)
+//                        for result in results as! [NSManagedObject] {
+//                            print("name - \(result.valueForKey("name")!)")
+//                        }
+//                } catch {
+//                    print(error)
+//                }
+//                
+//                return true
+//                }
+//            }
+//            
+//            
+//            //lang: String
+//            func getArticles (application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+//            {
+//                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Customer")
+//                do {
+//                    let results = try self.managedObjectContext.executeFetchRequest(fetchRequest)
+//                    for result in results as! [NSManagedObject] {
+//                        print("name - \(result.valueForKey("name")!)")
+//                    }
+//                } catch {
+//                    print(error)
+//                }
+//                
+//                //return true
+//                }
+//            }
+//    
+//    
+//            func removeArticle(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+//            {
+//                
+//            }
+//    
+//    
+//    //function to save my data
+//            func saveContext(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+//            {
+//                if managedObjectContext.hasChanges {
+//                    do {
+//                        try managedObjectContext.save()
+//                    } catch {
+//                        let nserror = error as NSError
+//                        NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+//                        abort()
+//                    }
+//                }
+//            }
+//            
+//}
+public class ArticleManager
+{
+    public var managedObjectContext: NSManagedObjectContext
+    
+    var bundle: Bundle;
+    var mom :NSManagedObjectModel
+    
+    public init()
+    {
+        bundle = Bundle(for: Article.self)
+        let url = bundle.url(forResource: "article", withExtension: "momd")!
+        mom = NSManagedObjectModel(contentsOf: url)!
+        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+        managedObjectContext = NSManagedObjectContext(concurrencyType:.mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = psc
+        
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docURL = urls[urls.endIndex-1]
+        let storeURL = docURL.appendingPathComponent("article.xcdatamodeld")
+        do {
+            try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+        } catch {
+            print("Error migrating store: \(error)")
+        }
+        
+    }
+    
+    public func newArticle(_ title:String, _ content:String,_ lang:String?,_ image:NSData?,_ atCreate:NSDate?, _ atModefied : NSDate? ) -> Article
+    {
+        let entity = NSEntityDescription.entity(forEntityName: "Article", in: managedObjectContext)
+        let article = NSManagedObject(entity: entity!,insertInto: managedObjectContext)
+        
+        article.setValue(title, forKey: "title")
+        article.setValue(content, forKey: "content")
+        article.setValue(lang, forKey: "lang")
+        article.setValue(image, forKey: "image")
+        article.setValue(atCreate, forKey: "atCreate")
+        article.setValue(atModefied, forKey: "atModefied")
+        
+        return article as! Article
+    }
+    
+    public func getAllArticles() -> [Article]
+    {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>()
+        
+        // Create Entity Description
+        let entityDescription = NSEntityDescription.entity(forEntityName: "Article", in: self.managedObjectContext)
+        
+        // Configure Fetch Request
+        fetchRequest.entity = entityDescription
+        
+        do {
+            let result = try self.managedObjectContext.fetch(fetchRequest)
+            return (result as? [Article])!
+            
+        } catch {
+            return []
+        }
+    }
+    
+    public func getArticles(withLang lang : String) -> [Article]
+    {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Article")
+        
+        // Create Predicate
+        let predicate = NSPredicate(format: "%K == %@", "lang", lang)
+        fetchRequest.predicate = predicate
+        
+        // Execute Fetch Request
+        do {
+            let result = try self.managedObjectContext.fetch(fetchRequest)
+            return (result as? [Article])!
+            
+        } catch {
+            _ = error as NSError
+            return []
+        }
+    }
+    
+    public func getArticles(containString str : String) -> [Article]
+    {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Article")
+        
+        // Create Predicate
+        let predicate = NSPredicate(format: "content CONTAINS %@ OR title CONTAINS %@", str)
+        fetchRequest.predicate = predicate
+        
+        // Execute Fetch Request
+        do {
+            let result = try self.managedObjectContext.fetch(fetchRequest)
+            return (result as? [Article])!
+            
+        } catch {
+            _ = error as NSError
+            return []
+        }
+        
+    }
+    
+    public func removeArticle(article : Article)
+    {
+        managedObjectContext.delete(article)
+    }
+    
+    public func save()
+    {
+        do {
+            try managedObjectContext.save()
+        }
+        catch {
+            print("Error save()")
+        }
+    }
+    
+}
--- mmitriuc2017/Example/mmitriuc2017/ViewController.swift
+++ mmitriuc2017/Example/mmitriuc2017/ViewController.swift
@@ -15,40 +15,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        func newArticle()
-//        {
-//            //creates a new item and returns it.
-//            let article1 = articleManager.newArticle()
-//            article1.title = "Article 1"
-//            article1.content = "First arctile day 08 "
-//            article1.creationDate = NSDate()
-//            article1.modificationDate = NSDate()
-//            article1.lang = "en"
-//            articleManager.save()
-//            
-//            let article2 = articleManager.newArticle()
-//            article1.title = "Article 2"
-//            article1.content = "Second arctile day 08 "
-//            article1.creationDate = NSDate()
-//            article1.modificationDate = NSDate()
-//            article1.lang = "en"
-//            articleManager.save()
-//            
-//            let article3 = articleManager.newArticle()
-//            article1.title = "Article 3"
-//            article1.content = "Third arctile day 08 "
-//            article1.creationDate = NSDate()
-//            article1.modificationDate = NSDate()
-//            article1.lang = "en"
-//            articleManager.save()
-//            
-//            print(articleManager.getAllArticles())
-//        }
         
-        //    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
+            func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
 //        // Dispose of any resources that can be recreated.
-//    }
+        }
+        }
     }
-}
-
